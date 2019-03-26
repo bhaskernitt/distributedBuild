3e60accb77e842b23ce30762b8416df4801af683
bhaskernitt <bhasker.nandkishor@gmail.com>
2019-03-26 23:07:06 +0530

    banner

--- src/main/java/com/bhaskerstreet/distributedbuild/DistributedbuildApplication.java
+++ src/main/java/com/bhaskerstreet/distributedbuild/DistributedbuildApplication.java
@@ -2,7 +2,10 @@ package com.bhaskerstreet.distributedbuild;
 
 import com.bhaskerstreet.distributedbuild.configuration.AppConfig;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Scanner;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -52,17 +55,35 @@ public class DistributedbuildApplication implements ApplicationRunner {
     @PostConstruct
     public void startupApplication() {
     	try {
-    	InputStream inputStream =this.getClass().getClassLoader().getSystemResourceAsStream("/distributedbuild/src/main/resources/startUpDesign");
-        
-    	byte bArray[]=inputStream.readAllBytes();
+    		
+    		logger.info(System.lineSeparator());
+    		ClassLoader classLoader = getClass().getClassLoader();
+    		StringBuilder result = new StringBuilder("");
+    		
+    		File file = new File(classLoader.getResource("startUpDesign").getFile());
+    		try (Scanner scanner = new Scanner(file)) {
+        		
+    			
+    			
+
+    			while (scanner.hasNextLine()) {
+    				String line = scanner.nextLine();
+    				result.append(line).append("\n");
+    			}
+
+    			scanner.close();
+    			logger.info("\n");
+    			
+    			logger.info(result.toString());
+
+    		} catch (IOException e) {
+    			e.printStackTrace();
+    		}
     	
-    	for (int i = 0; i < bArray.length; i++) {
-			System.out.print((char) bArray[i]);
-			
-		}
     	}
     	catch(Exception e) {
     		
+    		System.out.println();
     	}
     	
     }
